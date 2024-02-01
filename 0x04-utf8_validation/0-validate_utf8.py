b9440b7f59a2fc9b3a476ce6ddf355518c56112c
Serinajefthas <serina.jefthas@gmail.com>
2024-02-01 19:21:28 +0000

    fix errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,32 +2,32 @@
 """Module to check validity of utf-8 encoding"""
 
 
+def count_ones(byte):
+    """ counts leading 1s in byte"""
+    cnt = 0
+    mask = 1 << 7  # shifts bit rep of 1 seven pos to left
+    while byte & mask:
+        cnt += 1
+        mask >>= 1
+    return cnt
+
 
 def validUTF8(data):
-    """Function checks utf-8 validity of string"""
-    def is_byte(byte):
-        """checks if byte follows utf8 encoding rules"""
-        return 128 <= byte <= 191
-
-    def count_ones(byte):
-        """ counts leading 1s in byte"""
-        cnt = 0
-        mask = 1 << 7 #shifts bit rep of 1 seven pos to left
-        while byte & mask:
-            cnt += 1
-            mask >>= 1
-        return cnt
-
+    """checks if data is valid utf-8 encoding"""
+    num_bytes = 0
     i = 0
     while i < len(data):
-        num_bytes = count_ones(data[i])
-        if num_bytes == 1 or num_bytes > 4 or i + num_bytes > len(data):
-            return False #too many bites for utf8 rules
-
-        for j in range(1, num_bytes):
-            if not is_byte(data[i + j]):
+        if num_bytes == 0:
+            num_bytes = count_ones(data[i])
+            if num_bytes == 1 or num_bytes > 4:
+                return False  # too many bites for utf8 rules
+            if num_bytes == 0:
+                i += 1
+                continue
+        else:
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
                 return False
+        i += 1
+        num_bytes -= 1
 
-        i += num_bytes 
-
-    return True        
+    return num_bytes == 0
