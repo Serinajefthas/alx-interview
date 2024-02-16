fd7a711fa9ba95442f7f17b0491b32763f6695fc
Serinajefthas <serina.jefthas@gmail.com>
2024-02-16 21:39:22 +0000

    task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+const request = require('request');
+
+function printMovieCharacters(movieId) {
+    const apiUrl = `https://swapi-api.alx-tools.com/films/${movieId}`;
+
+    request(apiUrl, (error, response, body) => {
+        if (error) {
+            console.error('Error:', error);
+            return;
+        }
+
+        if (response.statusCode !== 200) {
+            console.error('Status:', response.statusCode);
+            return;
+        }
+
+        const movieData = JSON.parse(body);
+        const charactersUrls = movieData.characters;
+
+        // Fetch data for each character URL
+        charactersUrls.forEach(characterUrl => {
+            request(characterUrl, (charError, charResponse, charBody) => {
+                if (charError) {
+                    console.error('Error:', charError);
+                    return;
+                }
+
+                if (charResponse.statusCode !== 200) {
+                    console.error('Status:', charResponse.statusCode);
+                    return;
+                }
+
+                const characterData = JSON.parse(charBody);
+                console.log(characterData.name);
+            });
+        });
+    });
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,2 @@
+Task to manipulate and diplay data retrieved from Star Wars
+https://swapi-api.alx-tools.com/
