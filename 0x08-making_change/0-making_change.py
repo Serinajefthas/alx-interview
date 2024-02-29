5ac814e1f0945d3c3ba3251b71b7ed861499a416
Serinajefthas <serina.jefthas@gmail.com>
2024-02-29 22:12:30 +0000

    change method improved efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,35 +7,29 @@ def makeChange(coins, total):
     """Function to determine fewest number coins for total amt"""
     if total <= 0:
         return 0
+    coins.sort(reverse=true)
+    coin_cnt = 0
 
-    """set all values to pos infinity"""
+    for coin in coins:
+        if coin > total:
+            continue
+        cnt = total // coin
+        total -= cnt * coin
+        coin_cnt += cnt
+        if total == 0:
+            break
+    if total > 0:
+        return -1
+    return coin_cnt
+    """set all values to pos infinity
     minCoins = [float('inf')] * (total + 1)
     minCoins[0] = 0
 
     for coin in coins:
         for amt in range(coin, total + 1):
             minCoins[amt] = min(minCoins[amt], minCoins[amt - coin] + 1)
-    """if value still infinity then total not possible"""
+    if value still infinity then total not possible
     if minCoins[total] == float('inf'):
         return -1
-
-    """Works but less efficient
-    size = len(coins)
-
-    minCoins = [0 for i in range(total + 1)]
-    for i in range(1, total + 1):
-        initialise array values to infinity
-        minCoins[i] = sys.maxsize
-
-    for i in range(1, total + 1):
-        iterate through coins < i
-        for j in range(size):
-            if (coins[j] <= i):
-                sub_res = minCoins[i - coins[j]]
-                if (sub_res != sys.maxsize and
-                        sub_res + 1 < minCoins[i]):
-                    minCoins[i] = sub_res + 1
-
-    if minCoins[total] == sys.maxsize:
-        return -1"""
     return minCoins[total]
+"""
